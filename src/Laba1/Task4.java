68a119c9ba7ae54c87609ef758859e165ef45d13
IlyaChernaguzov <tana141345@yandex.ru>
2022-11-21 21:00:35 +0300

    add Task 1-6

---
+++ src/Laba1/Task4.java
@@ -0,0 +1,12 @@
+package Laba1;
+
+public class Task4 {
+    public static void main(String[] args) {
+        double x = 3.6;
+        double y = 4.1;
+        double z = 5.9;
+        double result = x*y*z;
+
+        System.out.println(result);
+    }
+}
---
+++ src/Laba1/Task5.java
@@ -0,0 +1,16 @@
+package Laba1;
+
+import java.util.Scanner;
+
+public class Task5 {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        int number1 = console.nextInt();
+        int number2 = console.nextInt();
+        int number3 = console.nextInt();
+
+        System.out.println(number1);
+        System.out.println(number2);
+        System.out.println(number3);
+    }
+}
