5e564641cd0656583f0a3a2310c564c72ea8d021
IlyaChernaguzov <tana141345@yandex.ru>
2022-11-21 20:38:47 +0300

    add task2

--- src/Laba1/Main.java
+++ src/Laba1/Main.java
@@ -2,9 +2,9 @@ package Laba1;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Я");
-        System.out.println("хорошо");
-        System.out.println("знаю");
-        System.out.println("Java.");
+        double rez = (46+10)*(10/2);
+        double rez2 = (29)*(4)*(-15);
+        System.out.println(rez);
+        System.out.println(rez2);
     }
 }
