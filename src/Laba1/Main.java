a966e5a4cf9e97326de4b5821832d57c3cc79417
IlyaChernaguzov <tana141345@yandex.ru>
2022-11-21 20:18:53 +0300

    add task1

--- src/Laba1/Main.java
+++ src/Laba1/Main.java
@@ -2,6 +2,9 @@ package Laba1;
 
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("Я");
+        System.out.println("хорошо");
+        System.out.println("знаю");
+        System.out.println("Java.");
     }
 }
