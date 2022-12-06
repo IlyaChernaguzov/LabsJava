cd208756463e633a134de38300c9f9440829774d
IlyaChernaguzov <tana141345@yandex.ru>
2022-12-06 23:07:52 +0300

    Add Laba7

--- src/Laba7/Main.java
+++ src/Laba7/Main.java
@@ -3,5 +3,11 @@ package Laba7;
 public class Main {
     public static void main(String[] args) {
 
+        Plain2.Wing wing1 = new Plain2.Wing(100);
+        Plain2.Wing wing2 = new Plain2.Wing(150);
+
+        wing1.printWeight();
+        wing2.printWeight();
+
     }
 }
